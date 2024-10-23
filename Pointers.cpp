ef51a5d8404cdd33929cb580f2a4d37abc344686
Abhirup-261004 <abhirupn2004@gmail.com>
2024-10-23 12:06:09 +0530

    Updated Pointers again

--- Pointers.cpp
+++ Pointers.cpp
@@ -5,7 +5,12 @@ using namespace std;
 int main(){
     int a=8;
     int *ptr= &a;
+
+    int **ptr2=&ptr;
     cout<< ptr << endl;
     cout<< &a<< endl;
+    cout<< endl;
+    cout<< &ptr<<endl;
+    cout<< ptr2<< endl;
     return 0;
 }
