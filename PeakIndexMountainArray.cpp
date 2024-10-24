22870788f2499f06c043bfee8cf694f9404a7a80
Abhirup-261004 <abhirupn2004@gmail.com>
2024-10-24 12:52:27 +0530

    Peak Index in Mountain Array

---
+++ PeakIndexMountainArray.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int peakIndex(vector<int>&nums){
+    int st=1,end=nums.size()-2;
+    while(st<=end){
+        int mid=st+(end-st)/2;
+        if(nums[mid-1]<nums[mid] && nums[mid]>nums[mid+1]){
+            return mid;
+        }
+
+        if(nums[mid-1]<nums[mid]){
+            st=mid+1;
+        }
+        else{
+            end=mid-1;
+        }
+    }
+    return -1;
+}
+
+int main(){
+    vector<int>nums={0,3,8,9,5,2};
+    cout<< peakIndex(nums)<< endl;
+    return 0;
+}
