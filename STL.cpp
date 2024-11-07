342f6b27b6c8cd23b34e1d34152a87d86babd5ec
Abhirup-261004 <abhirupn2004@gmail.com>
2024-11-07 23:36:27 +0530

    Push_back() & Emplace_back()

--- STL.cpp
+++ STL.cpp
@@ -9,5 +9,12 @@ int main(){
     for(auto p:vec){
         cout<<p.first<<" "<<p.second<<endl;
     }
+    cout<<endl;
+    vec.push_back({7,8});//just inserts the value as it is
+    vec.emplace_back(9,10);//in place objects create
+    for(auto p:vec){
+        cout<<p.first<<" "<<p.second<<endl;
+    }
+
     return 0;
 }
