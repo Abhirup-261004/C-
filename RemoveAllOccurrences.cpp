3518e3e1621919b31a1c6dda64aeccdf79afd616
Abhirup-261004 <abhirupn2004@gmail.com>
2024-11-14 11:58:18 +0530

    LeetCode-1910

---
+++ RemoveAllOccurrences.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+#include<bits/stdc++.h>
+using namespace std;
+
+string removeOccurrences(string s,string part){
+    while(s.length()>0 && s.find(part)<=s.length()){
+        s.erase(s.find(part),part.length());
+    }
+    return s;
+}
+
+int main(){
+    string s="daabcbaabcbc";
+    string part="abc";
+    cout<< "Updated string: "<< removeOccurrences(s,part);
+    return 0;
+}
