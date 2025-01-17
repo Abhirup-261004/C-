828cf505b5852ff93686838c88565d07af77832f
Abhirup-261004 <abhirupn2004@gmail.com>
2025-01-18 00:35:59 +0530

    LeetCode 189

---
+++ RotateArrayRight.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<vector>
+using namespace std; 
+
+void rotateArr(vector<int>&arr,int d){
+    int n=arr.size();
+    d=d%n; //To handle test cases where d > n
+    int a=0,b=n-d-1;
+    while(a<b){
+        swap(arr[a],arr[b]);
+        a++,b--;
+    }
+    b=n-d;
+
+    int end=n-1;
+
+    while(b<end){
+        swap(arr[b],arr[end]);
+        b++,end--;
+    }
+
+    int start=0;
+    end=n-1;
+
+    while(start<end){
+        swap(arr[start],arr[end]);
+        start++,end--;
+    }
+
+
+}
+
+int main(){
+    vector<int>arr={1,2,3,4,5,6,7};
+    int d=3;
+    rotateArr(arr,d);
+
+    for(auto c: arr){
+        cout<< c<< " ";
+    }
+
+    return 0;
+}
