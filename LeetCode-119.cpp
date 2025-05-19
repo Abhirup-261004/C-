34198b7495623096134cec6369a565f50691df3d
Abhirup-261004 <abhirupn2004@gmail.com>
2025-05-19 10:34:19 +0530

    Count inversions

---
+++ LeetCode-119.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+vector<int> getRow(int rowIndex) {
+    vector<int> res(1, 1);
+    long long prev = 1;
+    for(int k = 1; k <= rowIndex; k++) {
+        long long next_val = prev * (rowIndex - k + 1) / k;
+        res.push_back(next_val);
+        prev = next_val;
+    }
+    return res;
+}
+int main(){
+    int r=3;
+    <vector<int>ans=getRow(r);
+
+    for(int a:ans){
+        cout<< a;
+    }
+    cout<<endl;
+}
