d9d5816b7a970809ea0ef8171b69f8d04556d5a9
Abhirup-261004 <abhirupn2004@gmail.com>
2025-05-20 19:34:33 +0530

    LeetCode-3024 Triangle type

---
+++ LeetCode-3024.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+string triangleType(vector<int>& nums) {
+    set<int>s(nums.begin(),nums.end());
+    sort(nums.begin(),nums.end());
+    if(nums[0]+nums[1]<=nums[2]){
+        return "none";
+    }
+    if(s.size()==1){
+        return "equilateral";
+    }
+    if(s.size()==2){
+        return "isosceles";
+    }
+    return "scalene";
+}
+int main(){
+    
+}
