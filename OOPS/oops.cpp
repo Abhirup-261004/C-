3b1debbc110f60a7b0e0dd535a0ea6ac095a5d09
Abhirup-261004 <abhirupn2004@gmail.com>
2025-05-22 16:19:07 +0530

    OOPs

---
+++ OOPS/oops.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+ using namespace std;
+
+ class Teacher{
+    //Properties
+    string name;
+    string dept;
+    string subject;
+    double salary;
+
+    //Methods
+ }
+
+ int main(){
+
+ }
