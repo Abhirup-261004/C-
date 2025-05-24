af68cf6f6ff07aa116ccf5cf6f6bec9e8ce155f3
Abhirup-261004 <abhirupn2004@gmail.com>
2025-05-24 11:29:10 +0530

    LeetCode-219 Contain Duplicate 2

---
+++ LeetCode-219.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+
+using namespace std;
+
+bool containsNearbyDuplicate(vector<int>& nums, int k) {
+    unordered_map<int,int>mp;
+    for(int i=0;i<nums.size();i++){
+        if(mp.count(nums[i]) && abs(i-mp[nums[i]])<=k){
+            return true;
+        }
+        mp[nums[i]]=i;
+    }
+    return false;
+}
+
+int main(){
+    vector<int>nums={1,2,3,1};
+    int k=3;
+    cout<< containsNearbyDuplicate(nums,k)<<endl;
+
+    return 0;
+}
