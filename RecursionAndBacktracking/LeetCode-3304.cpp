6df78229a3b817d9dbd9f39dcbc638476b415783
Abhirup-261004 <abhirupn2004@gmail.com>
2025-06-03 18:09:32 +0530

    LeetCode - 3304 Kth string in string game

---
+++ RecursionAndBacktracking/LeetCode-3304.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+char generator(int k,string s){
+    if(s.size()>=k){
+        return s[k-1];
+    }
+    int n=s.size();
+    for(int i=0;i<n;i++){
+        s=s+char(s[i]+1);
+    }
+    return generator(k,s);
+}
+char kthCharacter(int k) {
+    string s="a";
+    return generator(k,s);
+}
+
+int main(){
+    int k=5;
+    cout<< kthCharacter(k)<<endl;
+
+    return 0;
+}
