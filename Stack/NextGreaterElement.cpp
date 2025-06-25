4642d9008193163f3dca545209e392420939a581
Abhirup-261004 <abhirupn2004@gmail.com>
2025-06-25 10:09:48 +0530

    NextGreaterElement

---
+++ Stack/NextGreaterElement.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<stack>
+#include<vector>
+using namespace std;
+
+int main(){
+    vector<int>arr={6,8,0,1,3};
+
+    //Next Greater Element
+    stack<int>s;
+    vector<int>ans;
+    int n=arr.size();
+    for(int i=n-1;i>=0;i--){
+        while(s.size()>0 && s.top()<=arr[i]){
+            s.pop();
+        }
+        if(s.empty()){
+            ans[i]=-1;
+        }else{
+            ans[i]=s.top();
+        }
+        s.push(arr[i]);
+    }
+
+    //print
+    for(int val:ans){
+        cout<<val<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
