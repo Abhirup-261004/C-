64d431f1bc29be064306076eccb195f75a961049
Abhirup-261004 <abhirupn2004@gmail.com>
2025-07-16 06:58:31 +0530

    LeetCode-3201 Find the maximum Length of Valid Subsequence I

---
+++ TwoPointers/LeetCode-3201.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int maximumLength(vector<int>& nums) {
+    int even=0,odd=0;
+    for(int num:nums){
+        if(num%2==0){
+            even++;
+        }else{
+            odd++;
+        }
+    }
+    int even_dp=0,odd_dp=0;
+    for(int num:nums){
+        if(num%2==0){
+            even_dp=max(even_dp,odd_dp+1);
+        }else{
+            odd_dp=max(odd_dp,even_dp+1);
+        }
+    }
+    return max({even,odd,even_dp,odd_dp});
+}
+
+int main(){
+    vector<int>nums={1,2,3,4};
+    cout<<maximumLength(nums)<<endl;
+    return 0;
+}
